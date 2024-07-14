a7479afafea30d5f399a1645f1611ed6a4692638
V1ruses <103028077+Virus1es@users.noreply.github.com>
2024-07-14 13:16:18 +0300

    изменения БД
    добавление таблиц и изменения существующих

--- DB/1_Create_DB_And_Tables.sql
+++ DB/1_Create_DB_And_Tables.sql
@@ -42,6 +42,12 @@ if OBJECT_ID('Purchases') is not null begin
 	print 'Удалена таблица Purchases';
 end;
 
+-- удалить старый вариант таблицы PubBooks(Книги с издательствами)
+if OBJECT_ID('PubBooks') is not null begin
+	drop table PubBooks;
+	print 'Удалена таблица PubBooks';
+end;
+
 -- удалить старый вариант таблицы Books(Книги)
 if OBJECT_ID('Books') is not null begin
 	drop table Books;
@@ -84,6 +90,12 @@ if OBJECT_ID('Cities') is not null begin
 	print 'Удалена таблица Cities';
 end;
 
+-- удалить старый вариант таблицы Providers(Поставщики)
+if OBJECT_ID('Providers') is not null begin
+	drop table Providers;
+	print 'Удалена таблица Providers';
+end;
+
 -- удалить старый вариант таблицы AgeRestrictions(Возрастные ограничения)
 if OBJECT_ID('AgeRestrictions') is not null begin
 	drop table AgeRestrictions;
@@ -94,6 +106,7 @@ end;
 -- Если осталась хотя бы одна таблица, откатить
 -- транзакцию
 if object_id('Genres') is not null  or 
+   object_id('Providers') is not null  or 
    object_id('AgeRestrictions') is not null  or 
    object_id('Authors') is not null  or 
    object_id('PublishingHouses') is not null or
@@ -101,6 +114,7 @@ if object_id('Genres') is not null  or
    object_id('Streets') is not null or
    object_id('Stores') is not null or
    object_id('Books') is not null or
+   object_id('PubBooks') is not null or
    object_id('Purchases') is not null or
    object_id('Sales') is not null  begin  
    rollback transaction
@@ -154,11 +168,24 @@ go
 
 -- создание таблицы Издательства
 create table PublishingHouses (
+     Id         int          not null identity(1, 1),  -- для автоинкремента
+	 [Name]     nvarchar(50) not null,                 -- название издательства
+	 Phone      nvarchar(15) not null,                 -- контактный номер
+	 AddPercent float        not null,                 -- наценка за издательство в процентах
+	 
+	 constraint PublishingHouses_PK primary key (Id),
+	 -- процент наценки за издательство должен быть в пределах от 0 до 100
+	 constraint PublishingHouses_AddPercent_check check (AddPercent between 0.0 and 100.0)
+);
+go
+
+-- создание таблицы Поставщики
+create table Providers (
      Id       int          not null identity(1, 1),  -- для автоинкремента
-	 [Name]   nvarchar(50) not null,                 -- название издательства
+	 [Name]   nvarchar(50) not null,                 -- название поставщика
 	 Phone    nvarchar(15) not null,                 -- контактный номер
 	 
-	 constraint PublishingHouses_PK primary key (Id)
+	 constraint Providers_PK primary key (Id)
 );
 go
 
@@ -198,26 +225,25 @@ create table Stores (
 );
 go
 
-
 -- создание таблицы Книги
 create table Books (
      Id            int          not null identity(1, 1) constraint Books_PK primary key (Id),
 	 Title         nvarchar(50) not null,                 -- название книги
 	 IdAuthor      int          not null,                 -- автор
 	 IdGenre       int          not null,                 -- жанр книги
-	 IdHouse       int          not null,                 -- издательство книги
 	 IdAge         int          not null,                 -- возрастное ограничение
 	 Price         int          not null,                 -- стоимость книги
 	 CreationYear  int          not null,                 -- год издания книги
+	 Rating        float        not null,                 -- рейтинг книги
 
 	 -- цена за книгу не может быть меньше 0
 	 constraint Books_Price_check check (Price > 0),
+	 -- рейтинг книги не должен быть отрицательным и не должен быть больше 5
+	 constraint Books_Rating_check check (Rating between 0.0 and 5.0),
 	 -- год создания книги не может быть больше текущего года или отрицательным
 	 constraint Books_CreationYear_check check (CreationYear between 0 and Year(getdate())),
 	 -- связь с таблицей Авторы
 	 constraint FK_Books_Authors foreign key(IdAuthor) references dbo.Authors(Id),
-	 -- связь с таблицей Издательства
-	 constraint FK_Books_PublishingHouses foreign key(IdHouse) references dbo.PublishingHouses(Id),
 	 -- связь с таблицей Возрастные ограничения
 	 constraint FK_Books_AgeRestrictions foreign key(IdAge) references dbo.AgeRestrictions(Id),
 	 -- связь с таблицей Жанры
@@ -225,45 +251,61 @@ create table Books (
 );
 go
 
+-- создание таблицы Книги с издательствами
+create table PubBooks (
+     Id      int not null identity(1, 1) constraint PubBooks_PK primary key (Id),  -- для автоинкремента
+	 IdBook  int not null,                 -- книга
+	 IdHouse int not null,                 -- издательство книги
+	 
+	 -- связь с таблицей Книги
+	 constraint FK_PubBooks_Books foreign key(IdBook) references dbo.Books(Id),
+	 -- связь с таблицей Издательства
+	 constraint FK_PubBooks_PublishingHouses foreign key(IdHouse) references dbo.PublishingHouses(Id)
+);
+go
+
 
 -- создание таблицы Закупки
 create table Purchases (
-     Id           int          not null identity(1, 1) constraint Purchases_PK primary key (Id),
-	 IdStore      int  not null,         -- в какой магазин закуплены книги
-	 IdBook       int  not null,         -- какую книгу закупили
-	 PurchaseDate date not null,         -- дата закупки
-	 Amount       int  not null,         -- количество закупленных книг
+     Id            int  not null identity(1, 1) constraint Purchases_PK primary key (Id),
+	 IdProvider    int  not null,         -- какой поставщик доставил книги
+	 IdPubBook     int  not null,         -- какую книгу закупили
+	 PurchaseDate  date not null,         -- дата закупки
+	 Amount        int  not null,         -- количество закупленных книг
+	 PurchasePrice int  not null,         -- цена закупки
 	 
 	 -- дата закупки не может быть больше текущей
 	 constraint Purchases_PurchaseDate_check check (PurchaseDate < getdate()),	 
+	 -- цена закупки не может быть отрицательной
+	 constraint Purchases_PurchasePrice_check check (PurchasePrice > 0),
 	 -- количество закуленных книг не может быть отрицательным
 	 constraint Purchases_Amount_check check (Amount > 0),
 	 -- связь с таблицей Магазины
-	 constraint FK_Purchases_Stores foreign key(IdStore) references dbo.Stores(Id),
+	 constraint FK_Purchases_Providers foreign key(IdProvider) references dbo.Providers(Id),
 	 -- связь с таблицей Книги
-	 constraint FK_Purchases_Books foreign key(IdBook) references dbo.Books(Id)
+	 constraint FK_Purchases_PubBooks foreign key(IdPubBook) references dbo.PubBooks(Id)
 );
 go
 
 -- создание таблицы Продажи
 create table Sales (
-     Id        int          not null identity(1, 1) constraint Sales_PK primary key (Id),
-	 IdBook    int  not null,        -- проданная книга
-	 IdStore   int  not null,        -- в каком магазине продана книга
-	 SaleDate  date not null,        -- дата продажи
-	 Amount    int  not null,        -- количество проданных книг
-	 SalePrice int  not null,        -- стоимость продажи (м.б. будет добавляться стоимость доставки)
+     Id           int          not null identity(1, 1) constraint Sales_PK primary key (Id),
+	 IdPubBook    int  not null,        -- проданная книга
+	 SaleDate     date not null,        -- дата продажи
+	 Amount       int  not null,        -- количество проданных книг
+	 SalePrice    int  not null,        -- стоимость продажи 
+	 DeliverPrice int  not null,        -- цена доставки
 	 
 	 -- дата закупки не может быть больше текущей
 	 constraint Sales_SaleDate_check check (SaleDate < getdate()),	 
 	 -- количество закуленных книг не может быть отрицательным
 	 constraint Sales_Amount_check check (Amount > 0),
+	 -- цена доставки не может быть отрицательной
+	 constraint Sales_DeliverPrice_check check (DeliverPrice > 0),
 	 -- стоимость продажи книг не может быть отрицательной
 	 constraint Sales_SalePrice_check check (SalePrice > 0),
 	-- связь с таблицей Книги
-	 constraint FK_Sales_Books foreign key(IdBook) references dbo.Books(Id),
-	-- связь с таблицей Магазины
-	 constraint FK_Sales_Stores foreign key(IdStore) references dbo.Stores(Id)
+	 constraint FK_Sales_PubBooks foreign key(IdPubBook) references dbo.PubBooks(Id)
 );
 go
 
@@ -272,12 +314,14 @@ go
 -- транзакцию
 if object_id('Genres') is null  or 
    object_id('AgeRestrictions') is null  or 
+   object_id('Providers') is null  or 
    object_id('Authors') is null  or 
    object_id('PublishingHouses') is null or
    object_id('Cities') is null or
    object_id('Streets') is null or
    object_id('Stores') is null or
    object_id('Books') is null or
+   object_id('PubBooks') is null or
    object_id('Purchases') is null or
    object_id('Sales') is null  begin  
    rollback transaction;
