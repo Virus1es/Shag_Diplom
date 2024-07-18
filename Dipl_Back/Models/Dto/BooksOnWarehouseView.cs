cc1ed1499dde446785a91c518775dcf007dd13ca
V1ruses <103028077+Virus1es@users.noreply.github.com>
2024-07-18 19:28:41 +0300

    Добавление классов для представлений

---
+++ Dipl_Back/Models/Dto/BooksOnWarehouseView.cs
@@ -0,0 +1,4 @@
+﻿namespace Dipl_Back.Models.Dto;
+
+// запись описывающая данные получаемы из представления Книги на складе
+public record BooksOnWarehouseView(int IdPubBook, string AuthorFullname, string Name, int RestAmount);
---
+++ Dipl_Back/Models/Dto/BooksWithFullPriceView.cs
@@ -0,0 +1,5 @@
+﻿namespace Dipl_Back.Models.Dto;
+
+// запись описывающая данные получаемы из представления Книги с издаельствами с вычисляемым полем стоимость
+public record BooksWithFullPriceView(string Title, string FirstName, string Surname, string Patronymic,
+                                     string GenreName, string Name, string AgeRange, double Rating, double FullPrice);
